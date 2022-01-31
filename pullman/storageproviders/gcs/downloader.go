e675b92d5718aca64b5253da0fd1b4bc538ca7cd
Paul Van Eck <pvaneck@us.ibm.com>
2022-01-31 11:29:32 -0800

    feat: Add GCS support to pullman (#10)
    * feat: Add GCS support to pullman
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>
    
    * Add authentication capabilities
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>
    
    * Some cleanup and refactoring
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>
    
    * Enable concurrent downloading
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>
    
    * Minor refactoring and improvements
    
    Signed-off-by: Paul Van Eck <pvaneck@us.ibm.com>

---
+++ pullman/storageproviders/gcs/downloader.go
@@ -0,0 +1,181 @@
+// Copyright 2021 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package gcsprovider
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+
+	"cloud.google.com/go/storage"
+	"github.com/go-logr/logr"
+	"github.com/kserve/modelmesh-runtime-adapter/pullman"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+type gcsClientFactory struct{}
+
+// gcsClientFactory implements gcsDownloaderFactory
+var _ gcsDownloaderFactory = (*gcsClientFactory)(nil)
+
+// gcsImplDownloader implements gcsDownloader
+var _ gcsDownloader = (*gcsImplDownloader)(nil)
+
+func (f gcsClientFactory) newDownloader(log logr.Logger, credentials map[string]string) (gcsDownloader, error) {
+	ctx := context.Background()
+	var cl *storage.Client
+	var err error
+
+	if len(credentials) > 0 {
+		credJson, marshalErr := json.Marshal(credentials)
+		if marshalErr != nil {
+			return nil, fmt.Errorf("unable to marshal json credentials: %w", marshalErr)
+		}
+		cl, err = storage.NewClient(ctx, option.WithCredentialsJSON(credJson))
+	} else {
+		cl, err = storage.NewClient(ctx, option.WithoutAuthentication())
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &gcsImplDownloader{
+		client: cl,
+		log:    log,
+	}, nil
+}
+
+type gcsImplDownloader struct {
+	client *storage.Client
+	log    logr.Logger
+
+	wg  sync.WaitGroup
+	mu  sync.Mutex
+	err error
+}
+
+func (d *gcsImplDownloader) listObjects(ctx context.Context, bucket string, prefix string) ([]string, error) {
+	query := &storage.Query{Prefix: prefix}
+	it := d.client.Bucket(bucket).Objects(ctx, query)
+
+	objectPaths := make([]string, 0, 10)
+
+	for {
+		obj, err := it.Next()
+		if err == iterator.Done {
+			return objectPaths, nil
+		}
+		if err != nil {
+			return nil, fmt.Errorf("GCS listObjects: unable to list bucket %q: %v", bucket, err)
+		}
+
+		if !d.shouldIgnoreObject(obj, prefix) {
+			objectPaths = append(objectPaths, obj.Name)
+		}
+	}
+}
+
+func (d *gcsImplDownloader) downloadBatch(ctx context.Context, bucket string, targets []pullman.Target) error {
+	ch := make(chan pullman.Target)
+
+	workerCount := maxDownloadConcurrency
+	if len(targets) < workerCount {
+		workerCount = len(targets)
+	}
+
+	d.wg.Add(workerCount)
+	for i := 0; i < workerCount; i++ {
+		go func() {
+			defer d.wg.Done()
+			if err := d.downloadFilesFromChannel(ctx, bucket, ch); err != nil {
+				d.setError(err)
+			}
+		}()
+	}
+
+	for _, target := range targets {
+		ch <- target
+	}
+
+	close(ch)
+	d.wg.Wait()
+
+	return d.err
+}
+
+func (d *gcsImplDownloader) downloadFilesFromChannel(ctx context.Context, bucket string, ch <-chan pullman.Target) error {
+	for {
+		target, ok := <-ch
+		if !ok {
+			break
+		}
+
+		if d.getError() != nil {
+			// Empty the rest of the channel if there is an error.
+			continue
+		}
+
+		file, err := pullman.OpenFile(target.LocalPath)
+		if err != nil {
+			return fmt.Errorf("unable to open local file '%s' for writing: %w", target.LocalPath, err)
+		}
+		defer file.Close()
+		d.log.V(1).Info("downloading object", "path", target.RemotePath, "filename", target.LocalPath)
+		reader, err := d.client.Bucket(bucket).Object(target.RemotePath).NewReader(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to create reader for object(%s) in bucket(%s): %v", target.RemotePath, bucket, err)
+		}
+		defer reader.Close()
+		if _, err = io.Copy(file, reader); err != nil {
+			return fmt.Errorf("failed to write data to file(%s): from object(%s) in bucket(%s): %v",
+				file.Name(), target.RemotePath, bucket, err)
+		}
+	}
+	return nil
+}
+
+func (d *gcsImplDownloader) getError() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	return d.err
+}
+
+func (d *gcsImplDownloader) setError(err error) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+	d.err = err
+}
+
+func (d *gcsImplDownloader) shouldIgnoreObject(object *storage.ObjectAttrs, prefix string) bool {
+	if object.Size == 0 {
+		d.log.V(1).Info("ignore downloading gcs object of 0 byte size", "gcs_path", object.Name)
+		return true
+	}
+	if strings.HasSuffix(object.Name, "/") {
+		d.log.V(1).Info("ignore downloading gcs object with trailing '/'", "gcs_path", object.Name)
+		return true
+	}
+	// If the path does not end with a slash, make sure we only get exact path matches. For example,
+	// with the prefix "models/mnist", we don't want to match path "models/mnist2/model.zip".
+	if !strings.HasSuffix(prefix, "/") && !strings.HasPrefix(object.Name, prefix+"/") && !(object.Name == prefix) {
+		d.log.V(1).Info("ignore downloading gcs object with prefix not followed by a slash", "prefix", prefix, "gcs_path", object.Name)
+		return true
+	}
+	return false
+}
---
+++ pullman/storageproviders/gcs/provider_test.go
@@ -0,0 +1,252 @@
+// Copyright 2021 IBM Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package gcsprovider
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"github.com/go-logr/logr"
+	"github.com/golang/mock/gomock"
+	"github.com/kserve/modelmesh-runtime-adapter/pullman"
+	"github.com/stretchr/testify/assert"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+)
+
+func newGCSProviderWithMocks(t *testing.T) (gcsProvider, *MockgcsDownloaderFactory, logr.Logger) {
+	mockCtrl := gomock.NewController(t)
+	mdf := NewMockgcsDownloaderFactory(mockCtrl)
+	g := gcsProvider{
+		gcsDownloaderFactory: mdf,
+	}
+	log := zap.New()
+	return g, mdf, log
+}
+
+func newGCSRepositoryClientWithMock(t *testing.T) (*gcsRepositoryClient, *MockgcsDownloader) {
+	mockCtrl := gomock.NewController(t)
+	md := NewMockgcsDownloader(mockCtrl)
+	log := zap.New()
+	gcsrc := gcsRepositoryClient{
+		gcsclient: md,
+		log:       log,
+	}
+	return &gcsrc, md
+}
+
+func Test_NewRepositoryWithCredentials(t *testing.T) {
+	g, mdf, log := newGCSProviderWithMocks(t)
+	c := pullman.NewRepositoryConfig("gcs", nil)
+
+	privateKey := "private key"
+	clientEmail := "client@email.com"
+	c.Set("private_key", privateKey)
+	c.Set("client_email", clientEmail)
+
+	// Test that credentials are passed to newDownloader if specified.
+	mdf.EXPECT().newDownloader(gomock.Any(), gomock.Eq(map[string]string{
+		"private_key": privateKey, "client_email": clientEmail, "type": "service_account"})).Times(1)
+
+	_, err := g.NewRepository(c, log)
+	assert.NoError(t, err)
+
+	tokenUri := "http://foo.bar"
+	c.Set("token_uri", tokenUri)
+
+	// Test that optional token_uri field is passed to newDownloader if specified.
+	mdf.EXPECT().newDownloader(gomock.Any(), gomock.Eq(map[string]string{
+		"private_key": privateKey, "client_email": clientEmail, "type": "service_account", "token_uri": tokenUri})).Times(1)
+
+	_, err = g.NewRepository(c, log)
+	assert.NoError(t, err)
+}
+
+func Test_NewRepositoryNoCredentials(t *testing.T) {
+	g, mdf, log := newGCSProviderWithMocks(t)
+	c := pullman.NewRepositoryConfig("gcs", nil)
+	mdf.EXPECT().newDownloader(gomock.Any(), gomock.Nil()).Times(1)
+
+	_, err := g.NewRepository(c, log)
+	assert.NoError(t, err)
+}
+
+func Test_Download_SimpleDirectory(t *testing.T) {
+	gcsRc, mdf := newGCSRepositoryClientWithMock(t)
+
+	bucket := "bucket"
+	c := pullman.NewRepositoryConfig("gcs", nil)
+	c.Set("bucket", bucket)
+
+	downloadDir := filepath.Join("test", "output")
+	inputPullCommand := pullman.PullCommand{
+		RepositoryConfig: c,
+		Directory:        downloadDir,
+		Targets: []pullman.Target{
+			{
+				RemotePath: "path/to/modeldir",
+			},
+		},
+	}
+
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("path/to/modeldir")).
+		Return([]string{"path/to/modeldir/file.ext", "path/to/modeldir/subdir/another_file"}, nil).
+		Times(1)
+
+	expectedTargets := []pullman.Target{
+		{
+			RemotePath: "path/to/modeldir/file.ext",
+			LocalPath:  filepath.Join(downloadDir, "file.ext"),
+		},
+		{
+			RemotePath: "path/to/modeldir/subdir/another_file",
+			LocalPath:  filepath.Join(downloadDir, "subdir", "another_file"),
+		},
+	}
+	mdf.EXPECT().downloadBatch(gomock.Any(), gomock.Eq(bucket), gomock.Eq(expectedTargets)).
+		Return(nil).
+		Times(1)
+
+	err := gcsRc.Pull(context.Background(), inputPullCommand)
+	assert.NoError(t, err)
+}
+
+func Test_Download_MultipleTargets(t *testing.T) {
+	gcsRc, mdf := newGCSRepositoryClientWithMock(t)
+
+	bucket := "bucket"
+	c := pullman.NewRepositoryConfig("gcs", nil)
+	c.Set("bucket", bucket)
+
+	downloadDir := filepath.Join("test", "output")
+	inputPullCommand := pullman.PullCommand{
+		RepositoryConfig: c,
+		Directory:        downloadDir,
+		Targets: []pullman.Target{
+			{
+				RemotePath: "dir",
+			},
+			{
+				// test that single file can be renamed
+				RemotePath: "some_file",
+				LocalPath:  "some_name.json",
+			},
+			{
+				// test that a directory can be "renamed"
+				RemotePath: "another_dir",
+				LocalPath:  "local_another_dir",
+			},
+			{
+				// test that single file can be renamed into subdirectory
+				RemotePath: "another_file",
+				LocalPath:  "local_another_dir/some_other_name.ext",
+			},
+			{
+				// test that single file can pulled into a target directory
+				RemotePath: "yet_another_file",
+				LocalPath:  "local_another_dir/",
+			},
+		},
+	}
+
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("dir")).
+		Return([]string{"dir/file1", "dir/file2"}, nil).
+		Times(1)
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("some_file")).
+		Return([]string{"some_file"}, nil).
+		Times(1)
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("another_dir")).
+		Return([]string{"another_dir/another_file", "another_dir/subdir1/subdir2/nested_file"}, nil).
+		Times(1)
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("another_file")).
+		Return([]string{"another_file"}, nil).
+		Times(1)
+	mdf.EXPECT().listObjects(context.Background(), gomock.Eq(bucket), gomock.Eq("yet_another_file")).
+		Return([]string{"yet_another_file"}, nil).
+		Times(1)
+
+	expectedTargets := []pullman.Target{
+		{
+			RemotePath: "dir/file1",
+			LocalPath:  filepath.Join(downloadDir, "file1"),
+		},
+		{
+			RemotePath: "dir/file2",
+			LocalPath:  filepath.Join(downloadDir, "file2"),
+		},
+		{
+			RemotePath: "some_file",
+			LocalPath:  filepath.Join(downloadDir, "some_name.json"),
+		},
+		{
+			RemotePath: "another_dir/another_file",
+			LocalPath:  filepath.Join(downloadDir, "local_another_dir", "another_file"),
+		},
+		{
+			RemotePath: "another_dir/subdir1/subdir2/nested_file",
+			LocalPath:  filepath.Join(downloadDir, "local_another_dir", "subdir1/subdir2/nested_file"),
+		},
+		{
+			RemotePath: "another_file",
+			LocalPath:  filepath.Join(downloadDir, "local_another_dir", "some_other_name.ext"),
+		},
+		{
+			RemotePath: "yet_another_file",
+			LocalPath:  filepath.Join(downloadDir, "local_another_dir", "yet_another_file"),
+		},
+	}
+	mdf.EXPECT().downloadBatch(gomock.Any(), gomock.Eq("bucket"), gomock.Eq(expectedTargets)).
+		Return(nil).
+		Times(1)
+
+	err := gcsRc.Pull(context.Background(), inputPullCommand)
+	assert.NoError(t, err)
+}
+
+func Test_GetKey(t *testing.T) {
+	provider := gcsProvider{}
+
+	createTestConfig := func() *pullman.RepositoryConfig {
+		config := pullman.NewRepositoryConfig("gcs", nil)
+		config.Set(configPrivateKey, "secret key")
+		config.Set(configClientEmail, "user@email.com")
+		return config
+	}
+
+	// should return the same result given the same config
+	t.Run("shouldMatchForSameConfig", func(t *testing.T) {
+		config1 := createTestConfig()
+		config2 := createTestConfig()
+
+		assert.Equal(t, provider.GetKey(config1), provider.GetKey(config2))
+	})
+
+	// changing the token_uri should change the key
+	t.Run("shouldChangeForTokenUri", func(t *testing.T) {
+		config1 := createTestConfig()
+		config2 := createTestConfig()
+		config2.Set(configTokenUri, "https://oauth2.googleapis.com/token2")
+
+		assert.NotEqual(t, provider.GetKey(config1), provider.GetKey(config2))
+	})
+
+	// changing the bucket should NOT change the key
+	t.Run("shouldNotChangeForBucket", func(t *testing.T) {
+		config1 := createTestConfig()
+		config2 := createTestConfig()
+		config2.Set(configBucket, "another_bucket")
+
+		assert.Equal(t, provider.GetKey(config1), provider.GetKey(config2))
+	})
+}
