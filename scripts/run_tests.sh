ad03d9a6bc1b6a664ca5b5208ef1915283b1460a
Christian Kadner <ckadner@us.ibm.com>
2023-10-12 09:49:29 -0700

    doc: Add developer instructions (#57)
    Add documentation that describes how to update the code, build a new
    image and test the code changes in a ModelMesh Serving deployment.
    
    ---------
    
    Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -15,13 +15,15 @@
 
 set -e
 
-## declare an array variable
-declare -a subpackages=("model-mesh-mlserver-adapter"
-                        "model-mesh-ovms-adapter"
-                        "model-mesh-torchserve-adapter"
-                        "model-mesh-triton-adapter"
-                        "model-serving-puller"
-                        "pullman")
+# declare an array variable with all sub-packages
+declare -a subpackages=(
+  "model-mesh-mlserver-adapter"
+  "model-mesh-ovms-adapter"
+  "model-mesh-torchserve-adapter"
+  "model-mesh-triton-adapter"
+  "model-serving-puller"
+  "pullman"
+)
 
 failed=false
 summary=""
